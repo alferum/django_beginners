6016717832dd12a04784fea6bd90bed1ffc55e44
Alf Alferum <alamzaker@yandex.ua>
2015-11-10 17:27:13 +0200

    add groups

--- students/views.py
+++ students/views.py
@@ -25,7 +25,12 @@ def students_delete(request, sid):
 # Views for Groups
 
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    groups = (
+        {'id': 1, 'title': 'МтМ-21', 'leader': u'Ячменев Олег'},
+        {'id': 2, 'title': 'МтМ-22', 'leader': u'Віталій Подоба'},
+        {'id': 3, 'title': 'МтМ-23', 'leader': u'Іванов Андрій'},
+        )
+    return render(request, 'students/groups_list.html', {'groups': groups})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
