6c019572b6b104abdc2dc3f5624dbd536c0ffce7
Alf Alferum <alamzaker@yandex.ua>
2015-12-16 18:41:06 +0200

    model journal

--- students/views/groups.py
+++ students/views/groups.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from ..models import Group
+from ..models.groups import Group
 
 def groups_list(request):
-    #groups = (
-    #    )
     groups = Group.objects.all()
     order_by = request.GET.get('order_by', '')
     if order_by in ('title', 'leader'):
--- students/views/journal.py
+++ students/views/journal.py
@@ -1,17 +1,29 @@
-# -*- coding: utf-8 -*-
-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from ..models.journal import Journal
 
 def journal(request):
-    students = (
-        {'id': 1, 's_name': u'Подоба Віталій'},
-        {'id': 2, 's_name': u'Корост Андрій'},
-        {'id': 3, 's_name': u'Притула Тарас'},
-        )
-    days = ['Ср 1', 'Чт 2', 'Пт 3', 'Сб 4', 'Нд 5', 'Пн 6', 'Вт 7', 'Ср 8', 'Чт 9', 'Пт 10', 'Сб 11', 'Нд 12', 'Пн 13', 'Вт 14', 'Ср 15', 'Чт 16', 
-            'Пт 17', 'Сб 18', 'Нд 19', 'Пн 20', 'Вт 21', 'Ср 22', 'Чт 23', 'Пт 24', 'Сб 25', 'Нд 26', 'Пн 27', 'Вт 28', 'Ср 29', 'Чт 30', 'Пт 31']
-    return render(request, 'students/journal.html', {'students': students, 'days': days})
+  
+    journals = Journal.objects.all()
+    checks = journals.values_list()
+    if checks: 
+        checks = [i[2:] for i in checks]
+    journals = zip(journals, checks)
+    
+    paginator = Paginator(journals, 3)
+    page = request.GET.get('page')
+    try:
+        journals = paginator.page(page)
+    except PageNotAnInteger:
+        journals = paginator.page(1)
+    except EmptyPage:
+        journals = paginator.page(paginator.num_pages)
+    
+    dates = [field.verbose_name for field in Journal._meta.fields[2:]]
+    
+    return render(request, 'students/journal.html', {'journals': journals, 'dates': dates })
     
 def journal_edit(request, sid):
     return HttpResponse('<h1>Edit Journal %s</h1>' % sid)
